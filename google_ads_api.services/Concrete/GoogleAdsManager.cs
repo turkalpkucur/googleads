c890ba96bb140fa70b5212900857efd827b4894a
Türkalp Kucur <turkalp.kucur@gmail.com>
2025-02-17 09:25:21 +0300

    list avaiable customers çalıştı
    list avaiable customers çalıştı

--- google_ads_api.services/Concrete/GoogleAdsManager.cs
+++ google_ads_api.services/Concrete/GoogleAdsManager.cs
@@ -75,62 +75,50 @@ namespace google_ads_api.services.Concrete
                     OAuth2Mode = Google.Ads.Gax.Config.OAuth2Flow.APPLICATION,
                     OAuth2ClientId = "124948101982-v7tnt3p8icvjmrm7f37n6kine232oli3.apps.googleusercontent.com",
                     OAuth2ClientSecret = "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",
-              //      OAuth2SecretsJsonPath = "credentials.json",
+                //    OAuth2SecretsJsonPath = "credentials.json",
                     OAuth2RefreshToken = refreshToken,
-                    LoginCustomerId = "4698070815"  
+                    LoginCustomerId = "4698070815"
                 };
-            GoogleAdsClient client = new GoogleAdsClient(config);
+                GoogleAdsClient client = new GoogleAdsClient(config);
 
 
-            Google.Ads.GoogleAds.V18.Services.GoogleAdsRow result = Run(client, Convert.ToInt64("4698070815"));
-            return result;
-        }
-            catch(Exception ex)
+                Google.Ads.GoogleAds.V18.Services.GoogleAdsRow result = Run(client, Convert.ToInt64("4698070815"));
+                return result;
+            }
+            catch (Exception ex)
             {
                 return null;
             }
-}
+        }
 
 
-public Google.Ads.GoogleAds.V18.Services.GoogleAdsRow Run(GoogleAdsClient client, long customerId)
-{
-    Google.Ads.GoogleAds.V18.Services.GoogleAdsRow result = new Google.Ads.GoogleAds.V18.Services.GoogleAdsRow();
-    // Get the GoogleAdsService.
-    Google.Ads.GoogleAds.V18.Services.GoogleAdsServiceClient googleAdsService = client.GetService(
-        Services.V18.GoogleAdsService);
+        public Google.Ads.GoogleAds.V18.Services.GoogleAdsRow Run(GoogleAdsClient client, long customerId)
+        {
 
-    // Create a query that will retrieve all campaigns.
-    string query = @"SELECT
-                    campaign.id,
-                    campaign.name,
-                    campaign.network_settings.target_content_network
-                FROM campaign
-                ORDER BY campaign.id";
-
-    try
-    {
-        // Issue a search request.
-        googleAdsService.SearchStream(customerId.ToString(), query,
-            delegate (Google.Ads.GoogleAds.V18.Services.SearchGoogleAdsStreamResponse resp)
+            try
             {
-                foreach (Google.Ads.GoogleAds.V18.Services.GoogleAdsRow googleAdsRow in resp.Results)
-                {
+                Google.Ads.GoogleAds.V18.Services.CustomerServiceClient customerService = client.GetService(Services.V18.CustomerService);
+                // Retrieve the list of customer resources.
+                string[] customerResourceNames = customerService.ListAccessibleCustomers();
 
-                    result = googleAdsRow;
+                // Display the result.
+                foreach (string customerResourceName in customerResourceNames)
+                {
+                    Console.WriteLine(
+                        $"Found customer with resource name = '{customerResourceName}'.");
                 }
             }
-        );
-        return result;
-    }
-    catch (GoogleAdsException e)
-    {
-        Console.WriteLine("Failure:");
-        Console.WriteLine($"Message: {e.Message}");
-        Console.WriteLine($"Failure: {e.Failure}");
-        Console.WriteLine($"Request ID: {e.RequestId}");
-        throw;
-    }
-}
+            catch (GoogleAdsException e)
+            {
+                Console.WriteLine("Failure:");
+                Console.WriteLine($"Message: {e.Message}");
+                Console.WriteLine($"Failure: {e.Failure}");
+                Console.WriteLine($"Request ID: {e.RequestId}");
+                throw;
+            }
+            return null;
+
+        }
 
 
     }
